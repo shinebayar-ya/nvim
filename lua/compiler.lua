0fe8ec6e931805b8931bbbc674c6e1b45db14913
shinebayar-ya <shinebayar00001@gmail.com>
2022-11-14 17:28:49 +0800

    Fixed compiler

--- lua/compiler.lua
+++ lua/compiler.lua
@@ -1,7 +1,1 @@
---vim.cmd(BufNewFile *.cpp execute "0r ~/coding/.template/template.cpp")
-vim.api.nvim_create_autocmd(
-    "BufNewFile",
-    "*.cpp"
-    -- command = "set tw=0"
-    --"0r ~/coding/.template/template.cpp"
-    )
+

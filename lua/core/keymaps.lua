e5d336e656afe4164c1d741e99b8d54d3fdb0921
shinebayar-ya <shinebayar00001@gmail.com>
2023-10-03 14:35:04 +0800

    first commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,50 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- Map <F8> to compile and run C++ code
+keymap.set('n', '<F8>', [[:w<CR>:!g++ -Wall -Wextra -std=c++17 -o %< % && ./%< <CR>]], { noremap = true})
+
+-- Map <F9> to compile and run C++ code with input file
+keymap.set('n', '<F9>', [[:w<CR>:!g++ -Wall -Wextra -std=c++17 -o %< % && ./%< < in <CR>]], { noremap = true})
+
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save it" })
+
+-- Do not yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G') 
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+ 
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('n', 's<left>', '<C-w>h')
+keymap.set('n', 's<up>', '<C-w>k')
+keymap.set('n', 's<down>', '<C-w>j')
+keymap.set('n', 's<right>', '<C-w>l')
+keymap.set('n', 'sh', '<C-w>h')
+keymap.set('n', 'sk', '<C-w>k')
+keymap.set('n', 'sj', '<C-w>j')
+keymap.set('n', 'sl', '<C-w>l')
